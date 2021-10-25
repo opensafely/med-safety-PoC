9330103ae2b4c76740b209b914880adfff049768
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-10-25 12:57:11 +0100

    feat: GIB01 / increased risk indicator implementation (modelled on PINCER GI bleed A)

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,7 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
 
+from codelists import *
+
 start_date = "2019-09-01"
 end_date = "2021-07-01"
 
@@ -47,5 +49,53 @@ study = StudyDefinition(
             "rate": "universal",
             "category": {"ratios": {"M": 0.5, "F": 0.5}},
         }
+    ),
+
+    #################################################################
+    #################################################################
+    ### INDICATORS (August 2019 )                                 ###
+    #################################################################
+    #################################################################
+
+
+    #################################################################
+    ### GI BLEED INDICATORS                                       ###
+    #################################################################
+
+    # GIB01 ======================================================= #
+
+    # GIB01 / increased risk indicator ---------------------------- #
+    # Patients aged 65 or over currently prescribed a non-steroidal
+    # anti-inflammatory drug (NSAID) without a gastro protective
+    # medicine and therefore potentially at increased risk of
+    # admission to hospital with a GI bleed.
+
+    oral_nsaid=patients.with_these_medications(
+        codelist=oral_nsaid_codelist,
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        between=["index_date - 3 months", "index_date"],
+    ),
+
+    ppi=patients.with_these_medications(
+        codelist=ulcer_healing_drugs_codelist,
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        between=["index_date - 3 months", "index_date"],
+    ),
+
+    indicator_GIB01_risk_denominator=patients.satisfying(
+        """
+        (NOT ppi) AND
+        (age >=65 AND age <=120)
+        """,
+    ),
+
+    indicator_GIB01_risk_numerator=patients.satisfying(
+        """
+        (NOT ppi) AND
+        (age >=65 AND age <=120) AND
+        oral_nsaid
+        """,
     ),
 )
