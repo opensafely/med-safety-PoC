598c41c692f1cddcdee6e3a681475212621c10e2
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-11-11 17:33:46 +0000

    feat: adding demographic variables to ce2 study definition

--- analysis/study_definition_ce2.py
+++ analysis/study_definition_ce2.py
@@ -21,6 +21,10 @@ emergency_admission_codes = [
            # - baby born at home as intended
     ]
 
+# Example study definitions for v2
+# https: // github.com/opensafely/SRO-Measures/blob/rebkwok/v2/analysis/study_definition.py
+# https: // github.com/opensafely/long-covid/blob/simplified-for-ce2/analysis/study_definition_cohort_v2.py
+
 
 class Cohort:
 
@@ -29,8 +33,47 @@ class Cohort:
     # There can be more than one registration per patient, get the latest one
     _latest_registrations = _registrations.latest("date_end")
     practice = _latest_registrations.get("pseudo_id")
+    population = _registrations.exists()
 
-    # https: // github.com/opensafely/SRO-Measures/blob/rebkwok/v2/analysis/study_definition.py
-    _died = table("deaths").filter("date_of_death",
-                                   on_or_before=index_date).exists()
-    population = _registrations.exists() & ~(_died.exists())
+    ### TO REVISIT: identify which patients are still alive
+
+    # Demographics ================================================ #
+    # Age --------------------------------------------------------- #
+    _age = table("patients").age_as_of(index_date)
+    _age_categories = {
+        "0-17": _age < 18,
+        "18-24": (_age >= 18) & (_age < 25),
+        "25-34": (_age >= 25) & (_age < 35),
+        "35-44": (_age >= 35) & (_age < 45),
+        "45-54": (_age >= 45) & (_age < 55),
+        "55-69": (_age >= 55) & (_age < 70),
+        "70-79": (_age >= 70) & (_age < 80),
+        "80+": _age >= 80,
+    }
+    age_group = categorise(_age_categories, default="missing")
+
+    # Sex
+    ### TO REVISIT: sex = table("patients").get("sex")
+
+    # Region
+    region = _latest_registrations.get("nuts1_region_name")
+
+    # IMD
+    _imd_value = table("patient_address").imd_rounded_as_of(index_date)
+    _imd_groups = {
+        "1": (_imd_value >= 1) & (_imd_value < (32844 * 1 / 5)),
+        "2": (_imd_value >= 32844 * 1 / 5) & (_imd_value < (32844 * 2 / 5)),
+        "3": (_imd_value >= 32844 * 2 / 5) & (_imd_value < (32844 * 3 / 5)),
+        "4": (_imd_value >= 32844 * 3 / 5) & (_imd_value < (32844 * 4 / 5)),
+        "5": (_imd_value >= 32844 * 4 / 5) & (_imd_value < 32844),
+    }
+    imd = categorise(_imd_groups, default="0")
+
+    # Ethnicity
+    ethnicity = (
+        table("clinical_events")
+        .filter("code", is_in=ethnicity_codes)
+        .filter("date", on_or_before=index_date)
+        .latest()
+        .get("code")
+    )
