513f924becfd79d422d5f67af4f6e2571c6c5e2b
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-11-16 13:40:02 +0000

    feat: saving for future reference, python script to move files (that didn't work!)

---
+++ analysis/move_dummy_data.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import os
+
+parser = ArgumentParser(description="Move dummy data")
+
+parser.add_argument(
+    "--origin",
+    help="Defining the path of the files to be moved",
+)
+
+
+parser.add_argument(
+    "--destination",
+    help="Defining where the files should be moved to",
+    type=Path
+)
+
+options = parser.parse_args()
+
+
+def existing_directory(value):
+    path = Path(value)
+    if not path.exists():
+        raise ValueError(f"{value} does not exit")
+    return path
+
+try:
+    existing_directory(options.destination)
+
+    mv_command = f"cp {options.origin} {options.destination}"
+
+    print(f"Executing [{mv_command}]")
+    os.system(mv_command)
+
+    rename_command_list = [f"for f in {options.destination}/*.csv",
+                        'do mv "$f" "${f/input/data}"',
+                        'done']
+    rename_command = "; ".join(rename_command_list)
+
+    print(f"Executing [{rename_command}]")
+    os.system(rename_command)
+
+except ValueError:
+    print(f"The destination directory '{options.destination}' does not exist")
