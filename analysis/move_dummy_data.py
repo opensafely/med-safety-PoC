b89f12d3ca303b3f8e392a60bdeeedfc836cbeef
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-11-16 17:19:44 +0000

    error: trying to add an action to move the dummy data files

--- analysis/move_dummy_data.py
+++ analysis/move_dummy_data.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
-import os
+import os, sys
 
 parser = ArgumentParser(description="Move dummy data")
 
@@ -8,7 +8,6 @@ parser.add_argument(
     "--origin",
     help="Defining the path of the files to be moved",
 )
-
 
 parser.add_argument(
     "--destination",
@@ -18,7 +17,6 @@ parser.add_argument(
 
 options = parser.parse_args()
 
-
 def existing_directory(value):
     path = Path(value)
     if not path.exists():
@@ -27,19 +25,20 @@ def existing_directory(value):
 
 try:
     existing_directory(options.destination)
-
-    mv_command = f"cp {options.origin} {options.destination}"
-
-    print(f"Executing [{mv_command}]")
-    os.system(mv_command)
-
-    rename_command_list = [f"for f in {options.destination}/*.csv",
-                        'do mv "$f" "${f/input/data}"',
-                        'done']
-    rename_command = "; ".join(rename_command_list)
-
-    print(f"Executing [{rename_command}]")
-    os.system(rename_command)
-
 except ValueError:
     print(f"The destination directory '{options.destination}' does not exist")
+    sys.exit()
+
+# mv_command = f"cp {options.origin} {options.destination}"
+
+# print(f"Executing [{mv_command}]")
+# os.system(mv_command)
+
+rename_command_list = [f"for f in {options.destination}/*.csv",
+                        'do echo mv "$f" "${f/input_/data_}"',
+                        'done']
+rename_command = "; ".join(rename_command_list)
+
+print(f"Executing [{rename_command}]")
+os.system(rename_command)
+
