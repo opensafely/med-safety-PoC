925dc92adf6669a85e5cd3dbd956e0246bb4217a
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2021-11-18 13:12:40 +0000

    feat: adding fake practice_id (using the number of GIB admissions) to the dummy data (while the real variable is exposed). This enables measures by practice.

--- analysis/study_definition_ce1_for_ce2.py
+++ analysis/study_definition_ce1_for_ce2.py
@@ -151,6 +151,17 @@ study = StudyDefinition(
         """
     ),
 
+    practice_id=patients.admitted_to_hospital(
+        with_these_primary_diagnoses=GIB_admissions_codelist,
+        with_admission_method=emergency_admission_codes,
+        returning="number_of_matches_in_period",
+        between=["index_date - 3 months", "index_date"],
+        return_expectations={
+            "incidence": 1.0,
+            "int": {"distribution": "normal", "mean": 8, "stddev": 3},
+        },
+    ),
+
 
     # # AKI01 ======================================================= #
 
